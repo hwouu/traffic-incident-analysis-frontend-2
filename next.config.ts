3c257c9fbc48cfd370f26741f931bd39a8a3e555
Hyunwoo <nhw3990@kau.kr>
2024-11-11 23:22:00 +0900

    chore: Next.js 설정 및 ESLint 규칙 수정

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  // Next.js 13 이상에서는 App Router가 기본값이므로 experimental.appDir 옵션 제거
 };
 
 export default nextConfig;
