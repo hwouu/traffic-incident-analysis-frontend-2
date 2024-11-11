5b325818121aab5d83bc9d43147c420e2e975e2f
Hyunwoo <nhw3990@kau.kr>
2024-11-11 23:22:35 +0900

    fix: 인증 레이아웃 컴포넌트 불필요한 import 제거

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import ThemeToggle from '../common/ThemeToggle';
 import { Swiper, SwiperSlide } from 'swiper/react';
